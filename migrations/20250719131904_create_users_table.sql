724805ade669dc1ea4014a53fd1628e0e173b6b6
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-07-19 18:12:16 +0330

    :card_file_box: Add default timestamps for created_at and updated_at of users table

--- migrations/20250719131904_create_users_table.sql
+++ migrations/20250719131904_create_users_table.sql
@@ -4,9 +4,9 @@ create table users(
     id bigint unsigned primary key auto_increment,
     username varchar(25) not null,
     password  varchar(25) not null,
-    created_at datetime not null,
-    updated_at datetime not null,
-    deleted_at datetime default NULL
+    created_at datetime not null default current_timestamp,
+    updated_at datetime not null default current_timestamp on update current_timestamp,
+    deleted_at datetime default null
 );
 -- +goose StatementEnd
 
