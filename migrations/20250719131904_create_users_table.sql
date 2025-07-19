f3e3276cf551807b3f995831ada4fffcc28fecb1
Sina Sadeghi <sina.sadeghi83@gmail.com>
2025-07-19 17:18:09 +0330

    :card_file_box: Remove redundant field for users table migrations

--- migrations/20250719131904_create_users_table.sql
+++ migrations/20250719131904_create_users_table.sql
@@ -4,8 +4,6 @@ create table users(
     id bigint unsigned primary key auto_increment,
     username varchar(25) not null,
     password  varchar(25) not null,
-    phone     varchar(12) not null unique, 
-    credits   int unsigned default 0,
     created_at datetime not null,
     updated_at datetime not null,
     deleted_at datetime default NULL
